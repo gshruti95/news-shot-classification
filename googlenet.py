5a60e152c4587e5654d61dd45f4e0634dfce845e
Shruti Gullapuram <shruti.gullapuram@gmail.com>
2016-07-22 13:17:08 -0700

    Fixed error bet pickle with chunks

--- googlenet.py
+++ googlenet.py
@@ -37,86 +37,57 @@ def googlenet(caffe_path, model_path, image_files):
 	mean_file = model_path + 'imagenet_mean.npy'
 	mu = np.load(mean_file).mean(1).mean(1)
 
-	# net = caffe.Net(model_prototxt,     # defines the structure of the model
-	#                 model_trained,  	# contains the trained weights
-	#                 caffe.TEST)
-
-	# transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
-	# transformer.set_transpose('data', (2,0,1))  # move image channels to outermost dimension
-	# transformer.set_mean('data', mu)            # subtract the dataset-mean value in each channel
-	# transformer.set_raw_scale('data', 255)      # rescale from [0, 1] to [0, 255]
-	# transformer.set_channel_swap('data', (2,1,0))
-	
-	net2 = caffe.Classifier(
+	net = caffe.Classifier(
             model_prototxt, model_trained,
             image_dims=(256, 256), raw_scale=255,
             mean=mu, channel_swap=(2, 1, 0))
 
 	# Assign batchsize
 	batch_size = 10
-	# data_blob_shape = net.blobs['data'].data.shape
-	# data_blob_shape = list(data_blob_shape)
-	# net.blobs['data'].reshape(batch_size, data_blob_shape[1], data_blob_shape[2], data_blob_shape[3])
-
-
 	count = 0
 
-	for image in image_files:
-		# print image_files[69]
+	# for image in image_files:
+	# 	# print image_files[69]
+	# 	bet = cPickle.load(open('/home/shruti/gsoc/caffehome/caffe/data/ilsvrc12/imagenet.bet.pickle'))
+
+	# 	input_image = caffe.io.load_image(image)	
+	# 	output = net.predict([input_image], oversample = True).flatten()
+	# 	count += 1
+
+	# 	bet['infogain'] -= np.array(bet['preferences']) * 0.1
+	# 	expected_infogain = np.dot(bet['probmat'], output[bet['idmapping']])
+	# 	expected_infogain *= bet['infogain']
+
+	# 	print len(expected_infogain)
+	# 	# sort the scores
+	# 	infogain_sort = expected_infogain.argsort()[::-1]
+	# 	bet_result = [(bet['words'][v], '%.5f' % expected_infogain[v]) for v in infogain_sort[:5]]
+
+	# 	print str(count) + ' bet result: %s', str(bet_result)
+
+	chunks_done = 0
+	for chunk in [image_files[x:x+batch_size] for x in xrange(0, len(image_files), batch_size)]:
+		print "Processing %.2f%% done ..." %((batch_size*chunks_done*100)/float(len(image_files)))
+		chunks_done = chunks_done + 1
+
 		bet = cPickle.load(open('/home/shruti/gsoc/caffehome/caffe/data/ilsvrc12/imagenet.bet.pickle'))
 
-		input_image = caffe.io.load_image(image)	
-		another = net2.predict([input_image], oversample = True).flatten()
-		count += 1
-
-		bet['infogain'] -= np.array(bet['preferences']) * 0.1
-		expected_infogain = np.dot(bet['probmat'], another[bet['idmapping']])
-		expected_infogain *= bet['infogain']
-
-		print len(expected_infogain)
-		# sort the scores
-		infogain_sort = expected_infogain.argsort()[::-1]
-		bet_result = [(bet['words'][v], '%.5f' % expected_infogain[v]) for v in infogain_sort[:5]]
-
-		print str(count) + ' bet result: %s', str(bet_result)
-
-	# scores = None
-	# chunks_done = 0
-	# for chunk in [image_files[x:x+batch_size] for x in xrange(0, len(image_files), batch_size)]:
-	# 	print "Processing %.2f%% done ..." %((batch_size*chunks_done*100)/float(len(image_files)))
-	# 	chunks_done = chunks_done + 1
-
-	# 	# if len(chunk) < batch_size:
-	# 		# net.blobs['data'].reshape(len(chunk), data_blob_shape[1], data_blob_shape[2], data_blob_shape[3])
-
-	# 	# net.blobs['data'].data[...] = map(lambda y: transformer.preprocess('data', caffe.io.load_image(y)), chunk)
-	# 	input_images = map(lambda y: caffe.io.load_image(y), chunk)
-	# 	print chunk
-	# 	# input_image = caffe.io.load_image(image_files[0])	
-	# 	# print input_image
-	# 	# output = net.forward()
-	# 	another = net2.predict(input_images, oversample = True).flatten()
-	# 	print len(another), type(another)
+		input_images = map(lambda y: caffe.io.load_image(y), chunk)
+		output = net.predict(input_images, oversample = True).flatten()
 		
-	# 	# places_labels = model_path + 'synset_words.txt'
-	# 	# labels = np.loadtxt(places_labels, str, delimiter='\t')
-		
-	# 	print len(another[bet['idmapping']])
+		vect = 1000
+		for single_output in [output[k:k+vect] for k in xrange(0,len(output),vect)]:
 
-	# 	vect = 1000
-	# 	for another_img in [another[k:k+vect] for k in xrange(0,len(another),vect)]:
+			count += 1
+			bet['infogain'] -= np.array(bet['preferences']) * 0.1
+			expected_infogain = np.dot(bet['probmat'], single_output[bet['idmapping']])
+			expected_infogain *= bet['infogain']
 
-	# 		count += 1
-	# 		bet['infogain'] -= np.array(bet['preferences']) * 0.1
-	# 		expected_infogain = np.dot(bet['probmat'], another_img[bet['idmapping']])
-	# 		expected_infogain *= bet['infogain']
+			# sort the scores
+			infogain_sort = expected_infogain.argsort()[::-1]
+			bet_result = [(bet['words'][v], '%.5f' % expected_infogain[v]) for v in infogain_sort[:5]]
 
-	# 		print len(expected_infogain)
-	# 		# sort the scores
-	# 		infogain_sort = expected_infogain.argsort()[::-1]
-	# 		bet_result = [(bet['words'][v], '%.5f' % expected_infogain[v]) for v in infogain_sort[:5]]
-
-	# 		print str(count) + ' bet result: %s', str(bet_result)
+			print str(count) + ' bet result: %s', str(bet_result)
 
 
 
@@ -126,10 +97,10 @@ def googlenet(caffe_path, model_path, image_files):
 		# 	scores = {}
 		# 	scores['prob'] = output['prob'].copy()
 		# 	# allout = {}
-		# 	# allout = another.copy()
+		# 	# allout = output.copy()
 		# else:
 		# 	scores['prob'] = np.vstack((scores['prob'], output['prob']))
-		# 	# allout = np.vstack((allout, another))
+		# 	# allout = np.vstack((allout, output))
 			
 	# print "images len: ", len(image_files)
 	# for idx, output_prob in enumerate(scores['prob']):
