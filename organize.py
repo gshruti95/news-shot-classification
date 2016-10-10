8a95a2aaf5a30ef16f29bf0bd4b8a29b3d3e6509
Shruti Gullapuram <shruti.gullapuram@gmail.com>
2016-10-10 23:48:16 +0530

    organize

--- organize.py
+++ organize.py
@@ -102,25 +102,28 @@ def main():
 
 	else:
 
-		clip_path = sys.argv[1] 								## ../../dir/video.mp4
-		rel_clip_path = clip_path.rsplit('/',1)[0] + '/'		## ../../dir/
-		clip_name = clip_path.rsplit('/',1)[1]					## video.mp4
-		clip = clip_name.rsplit('.',1)[0]						## video
-		output_filename = clip 									## video
-		clip_dir = rel_clip_path								## ../../dir/
+		# clip_path = sys.argv[1] 								## ../../dir/video.mp4
+		# rel_clip_path = clip_path.rsplit('/',1)[0] + '/'		## ../../dir/
+		# clip_name = clip_path.rsplit('/',1)[1]					## video.mp4
+		# clip = clip_name.rsplit('.',1)[0]						## video
+		# output_filename = clip 									## video
+		# clip_dir = rel_clip_path								## ../../dir/
 
+		source = os.listdir('/home/sxg755/trainset/')
 		temp = '/home/sxg755/dataset/train/new_all_frames/'		## ../../dir/all_frames/
 		if not os.path.exists(temp):
 			os.makedirs(temp)
 
-		keyframe_times = keyframes.keyframes(temp, clip_path)
-		keyframes_list = fileops.get_keyframeslist(temp, clip_path)
+		for file in source:
+			if file.endswith('.mp4'):
+				keyframe_times = keyframes.keyframes(temp, clip_path)
+				keyframes_list = fileops.get_keyframeslist(temp, clip_path)
 
-		image_files = cropframes.cropframes(temp, keyframes_list, clip_path)
-		image_files = [image_file.rsplit('/',1)[1] + '\n' for image_file in image_files]
+				image_files = cropframes.cropframes(temp, keyframes_list, clip_path)
+				image_files = [image_file.rsplit('/',1)[1] + '\n' for image_file in image_files]
 
-		with open(temp + 'keyframes_list.txt', 'aw') as file:
-			file.writelines(image_files)
+				with open(temp + 'keyframes_list.txt', 'aw') as file:
+					file.writelines(image_files)
 
 
 if __name__ == '__main__':
